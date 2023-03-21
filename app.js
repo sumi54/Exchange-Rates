9fed968a1b5bc5b90eadf5c582a46ea89fd2c6ef
sumi54 <sumeyyekarakullukcu34@gmail.com>
2023-03-21 16:37:05 +0300

    arayüze ekleme

--- app.js
+++ app.js
@@ -2,19 +2,24 @@ let amountElement=document.querySelector("#amount");
 let firstSelect=document.querySelector("#firstCurrency");
 let secondSelect=document.querySelector("#secondCurrency");
 let currency=new Currency("USD","TRY");
+let ui=new UI(firstSelect,secondSelect);
  eventListeners();
  function eventListeners(){
     amountElement.addEventListener("input",exchangeCurrency);
     firstSelect.onchange=function(){
         currency.changeFirstCurrency(firstSelect.options[firstSelect.selectedIndex].textContent);
+        ui.changeFirst();
     };
     secondSelect.onchange=function(){
         currency.changeSecondCurrency(secondSelect.options[secondSelect.selectedIndex].textContent);
+        ui.changeSecond();
     };
  }
  function exchangeCurrency(){
     currency.changeAmount(amountElement.value);
     currency.exchange()
-    .then(result=>console.log(result))
+    .then(result=>{
+        ui.displayResult(result);
+    })
     .catch(err=>console.log(err));
  }
